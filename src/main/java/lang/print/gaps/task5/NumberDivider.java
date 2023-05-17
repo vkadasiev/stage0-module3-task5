e4780f0f47a6e0f59801ea072a1bd572192a49b4
Vardan <vkadasiev@gmail.com>
2023-05-17 12:21:18 +0400

    Tasks completed

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        int divided = toBeDivided / 5;
+        System.out.println(toBeDivided + " divided by 5 is equal to " + divided );
     }
-}
+        public static void main(String[] args) {
+            NumberDivider divide = new NumberDivider();
+            divide.divide (20);
+        }
+
+    }
+
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        float seconds = minutes * 60;
+        System.out.println(minutes + " minutes is equal to " + seconds + " seconds.");
+    }
+
+    public static void main(String[] args) {
+        TimeConvertor convertor = new TimeConvertor();
+        convertor.convert(5.7f);
     }
 }
+
+
